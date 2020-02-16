cc7ed745af286ded8b3596520af93691d72e5ee2
Administrator <Administrator@DESKTOP-099UQRG>
2020-02-16 12:41:54 -0500

    Add Javadoc, add / default endpoint, spruce up start.html

---
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -0,0 +1,26 @@
+package com.plantplaces;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PlantPlacesController {
+	
+	/**
+	 * Handle the /start endpoint
+	 * @return
+	 */
+	@RequestMapping("/start")
+	public String start() {
+		return "start";
+	}
+	
+	/**
+	 * Handle the / default endpoint
+	 * @return
+	 */
+	@RequestMapping("/")
+	public String index() {
+		return "start";
+	}
+}
